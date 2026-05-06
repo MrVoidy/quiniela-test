ce9522f824e41c1206b0eb0e6f2f75423da0b74a
MrVoidy <joseaugustocortezteran@gmail.com>
2026-05-06 17:36:24 -0400

    more tests

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id BINARY(16) PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    api_key VARCHAR(64) UNIQUE NOT NULL
+);
